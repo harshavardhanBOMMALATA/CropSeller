86eb3e6415ea540af085ccfd02621396d8c5ad61
HARSHAVARDHAN_BOMMALATA <167017461+harshavardhanBOMMALATA@users.noreply.github.com>
2026-01-18 10:36:27 +0530

    Fix Static Files for Render

--- cropseller/settings.py
+++ cropseller/settings.py
@@ -39,7 +39,7 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-local-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -144,6 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
